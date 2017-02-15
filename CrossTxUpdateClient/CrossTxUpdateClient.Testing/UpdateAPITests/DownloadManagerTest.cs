3fc16de0dff1c079b1a6171934ca68f4c3cfec34
Lane Lutgen <lutgenlane@gmail.com>
2017-02-14 17:57:10 -0700

    Added base code for downloading the zip files (not complete)

---
+++ CrossTxUpdateClient/CrossTxUpdateClient.Testing/UpdateAPITests/DownloadManagerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrossTxUpdateClient.UpdateAPI;
+
+namespace CrossTxUpdateClient.Testing.UpdateAPITests
+{
+    [TestClass]
+    public class DownloadManagerTest
+    {
+        /// <summary>
+        /// This method is for testing that that csv files are downloaded successfully 
+        /// </summary>
+        [TestMethod]
+        public void DownloadCSVFile_FileDownloadSuccessful()
+        {
+            DownloadManager dlManager = new DownloadManager("C:\\Users\\Lane\\Desktop\\csv.zip");
+            dlManager.DownloadUpdateFile();
+        }
+
+        /// <summary>
+        /// This method is for testing that that update files are downloaded successfully 
+        /// </summary>
+        [TestMethod]
+        public void DownloadUpdateFile_FileDownloadSuccessful()
+        {
+            DownloadManager dlManager = new DownloadManager("C:\\Users\\Lane\\Desktop\\update.zip");
+            dlManager.DownloadUpdateFile();
+        }
+
+        /// <summary>
+        /// This method is for testing that that deactivation files are downloaded successfully 
+        /// </summary>
+        [TestMethod]
+        public void DownloadDeactivationFile_FileDownloadSuccessful()
+        {
+            DownloadManager dlManager = new DownloadManager("C:\\Users\\Lane\\Desktop\\deactivation.zip");
+            dlManager.DownloadDeactivationFile();
+        }
+    }
+}
---
+++ CrossTxUpdateClient/CrossTxUpdateClient/UpdateAPI/DownloadManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossTxUpdateClient.UpdateAPI
+{
+    public class DownloadManager
+    {
+        
+        public string FilePath
+        {
+            get
+            {
+                return filePath;
+            }
+            set
+            {
+                filePath = value;
+            }
+        }
+
+        private string filePath;
+
+        //Note: these are hard coded right now but we will have to write the logic to change them to most recent
+        private const string csvURL = "http://download.cms.gov/nppes/NPPES_Data_Dissemination_January_2017.zip";
+        private const string updateURL = "http://download.cms.gov/nppes/NPPES_Data_Dissemination_020617_021217_Weekly.zip";
+        private const string deactivationURL = "http://download.cms.gov/nppes/NPPES_Deactivated_NPI_Report_011017.zip";   
+
+        public DownloadManager()
+        {
+            filePath = null;
+        }
+
+        public DownloadManager(string path)
+        {
+            filePath = path;
+        }
+
+        public void DownloadFullCSV()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(CSVDownload_Complete);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(CSVDownload_ProgressChanged);
+
+            //This can throw a WebException but we don't want to catch it here
+            webClient.DownloadFile(new Uri(csvURL), filePath);
+        }
+
+        private void CSVDownload_Complete(object sender, AsyncCompletedEventArgs e)
+        {
+            //Handler for download completion
+            Console.WriteLine("CSV File Download Complete");
+        }
+
+        private void CSVDownload_ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //This function can be used for a progress bar
+            int progress = e.ProgressPercentage;
+            Console.WriteLine(progress);
+        }
+
+        public void DownloadUpdateFile()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(UpdateFileDownload_Complete);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(UpdateFileDownload_ProgressChanged);
+
+            //This can throw a WebException but we don't want to catch it here
+            webClient.DownloadFile(new Uri(updateURL), filePath);
+        }
+
+        private void UpdateFileDownload_Complete(object sender, AsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Update File Download Complete");
+        }
+
+        private void UpdateFileDownload_ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            int progress = e.ProgressPercentage;
+            Console.WriteLine(progress);
+        }
+
+        public void DownloadDeactivationFile()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DeactivationDownload_Complete);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DeactivationDownload_ProgressChanged);
+
+            //This can throw a WebException but we don't want to catch it here
+            webClient.DownloadFile(new Uri(deactivationURL), filePath);
+        }
+
+        private void DeactivationDownload_Complete(object sender, AsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Deactivation File Download Complete");
+        }
+
+        private void DeactivationDownload_ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            int progress = e.ProgressPercentage;
+            Console.WriteLine(progress);
+        }
+    }
+}
