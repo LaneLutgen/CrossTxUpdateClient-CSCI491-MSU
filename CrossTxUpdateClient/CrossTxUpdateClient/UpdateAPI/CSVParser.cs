71c2e86a3567d085b5819f2b86f12a784f76be09
ben-rhuman <benrhuman@gmail.com>
2017-03-19 17:04:21 -0600

    Added parser, recipe design pattern
    Added parser, recipe design pattern

---
+++ CrossTxUpdateClient/CrossTxUpdateClient/UpdateAPI/CSVParser.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossTxUpdateClient.UpdateAPI
+{
+    abstract class CSVParser
+    {
+
+        protected string filePath;
+
+        public CSVParser()
+        {
+        }
+
+        public void diffEntry() //Compares the new entry to determine if it needs to be replaced or inserted 
+        {
+            //If found in DB, replace entry
+            if (inDB())
+            {
+                replaceEntry();
+            }
+            else //Else
+            {
+                insertEntry();
+            }
+        }
+
+        abstract public void insertEntry(); //Inserts if NPI does not exist
+        abstract public void replaceEntry(); //Replaces if NPI exists       
+       
+
+
+    }
+
+    class Activate : CSVParser
+    {
+
+        public Activate(string path) //Path leading to .csv 
+        {
+            base.filePath = path;
+        }
+
+        public override void insertEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void replaceEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    class Deactivate : CSVParser
+    {
+        public Deactivate(string path) //Path leading to .csv 
+        {
+            base.filePath = path;
+        }
+
+        public override void insertEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void replaceEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    class Initialize : CSVParser
+    {
+        public Initialize(string path) //Path leading to .csv 
+        {
+            base.filePath = path;
+        }
+
+        public override void insertEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void replaceEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
