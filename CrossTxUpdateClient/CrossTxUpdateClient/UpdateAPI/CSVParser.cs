b5d1425a54b9b77ab52bd930254a840c9d82267c
ben-rhuman <benrhuman@gmail.com>
2017-03-19 17:45:03 -0600

    Changed parser structure

--- CrossTxUpdateClient/CrossTxUpdateClient/UpdateAPI/CSVParser.cs
+++ CrossTxUpdateClient/CrossTxUpdateClient/UpdateAPI/CSVParser.cs
@@ -6,87 +6,35 @@ using System.Threading.Tasks;
 
 namespace CrossTxUpdateClient.UpdateAPI
 {
-    abstract class CSVParser
+    interface CSVParser<T>
     {
-
-        protected string filePath;
-
-        public CSVParser()
-        {
-        }
-
-        public void diffEntry() //Compares the new entry to determine if it needs to be replaced or inserted 
-        {
-            //If found in DB, replace entry
-            if (inDB())
-            {
-                replaceEntry();
-            }
-            else //Else
-            {
-                insertEntry();
-            }
-        }
-
-        abstract public void insertEntry(); //Inserts if NPI does not exist
-        abstract public void replaceEntry(); //Replaces if NPI exists       
-       
-
-
+        void Parse();
     }
 
-    class Activate : CSVParser
+    public class Activate : CSVParser<Activate>
     {
-
+        string filePath; 
         public Activate(string path) //Path leading to .csv 
         {
-            base.filePath = path;
+            filePath = path;
         }
 
-        public override void insertEntry()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void replaceEntry()
+        void CSVParser<Activate>.Parse()
         {
             throw new NotImplementedException();
         }
 
     }
 
-    class Deactivate : CSVParser
+    class Deactivate : CSVParser<Deactivate>
     {
+        string filePath;
         public Deactivate(string path) //Path leading to .csv 
         {
-            base.filePath = path;
+           filePath = path;
         }
 
-        public override void insertEntry()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void replaceEntry()
-        {
-            throw new NotImplementedException();
-        }
-
-    }
-
-    class Initialize : CSVParser
-    {
-        public Initialize(string path) //Path leading to .csv 
-        {
-            base.filePath = path;
-        }
-
-        public override void insertEntry()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void replaceEntry()
+        void CSVParser<Deactivate>.Parse()
         {
             throw new NotImplementedException();
         }
