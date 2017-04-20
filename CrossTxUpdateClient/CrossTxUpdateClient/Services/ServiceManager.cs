b54640fff90de9c53ef3686b649e8f9849f5a38a
Lane Lutgen <lutgenlane@gmail.com>
2017-04-20 09:19:05 -0600

    Added 24 hour timer object

--- CrossTxUpdateClient/CrossTxUpdateClient/Services/ServiceManager.cs
+++ CrossTxUpdateClient/CrossTxUpdateClient/Services/ServiceManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hardcodet.Wpf.TaskbarNotification;
+using System.Timers;
 
 namespace CrossTxUpdateClient.Services
 {
@@ -12,14 +13,27 @@ namespace CrossTxUpdateClient.Services
     /// </summary>
     public static class ServiceManager
     {
+        private static Timer timer;
+
         public static void Start()
         {
+            //24 hour timer (final implementation)
+            //timer = new Timer(24 * 60 * 60 * 1000);
 
+            //5 minutes for testing purposes
+            timer = new Timer(5 * 60 * 1000);
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            //Code to check for new links
         }
 
         public static void Stop()
         {
-
+            timer.Stop();
+            timer = null;
         }
     }
 }
