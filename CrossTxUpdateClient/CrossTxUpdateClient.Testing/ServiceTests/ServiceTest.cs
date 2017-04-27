dd4ef578afd909473f3fda9c5492852f19bff6bb
Lane Lutgen <lutgenlane@gmail.com>
2017-04-26 19:02:51 -0600

    Added a couple more service manager tests

--- CrossTxUpdateClient/CrossTxUpdateClient.Testing/ServiceTests/ServiceTest.cs
+++ CrossTxUpdateClient/CrossTxUpdateClient.Testing/ServiceTests/ServiceTest.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CrossTxUpdateClient.Services;
+using CrossTxUpdateClient.UpdateAPI;
 
 namespace CrossTxUpdateClient.Testing.ServiceTests
 {
@@ -22,10 +23,40 @@ namespace CrossTxUpdateClient.Testing.ServiceTests
         }
 
         [TestMethod]
+        public void CheckForNewUpdates_Test_WithLink()
+        {
+            DownloadManager mngr = DownloadManager.CreateAndGetInstance("blah", "blah"); 
+            DataTable table = new DataTable();
+            table.Columns.Add("Link");
+            table.Columns.Add("Date");
+            table.Columns.Add("Type");
+            table.Rows.Add(DownloadManager.UpdateURL, DateTime.Now.ToString(), "Update");
+
+            ServiceManager.Start(table);
+            bool found = ServiceManager.CheckForNewUpdates();
+            Assert.IsTrue(found);
+        }
+
+        [TestMethod]
         public void CheckForNewDeactivations_Test()
         {
             bool found = ServiceManager.CheckForNewDeactivations();
             Assert.IsFalse(found);
+        }
+
+        [TestMethod]
+        public void CheckForNewDeactivations_Test_WithLink()
+        {
+            DownloadManager mngr = DownloadManager.CreateAndGetInstance("blah", "blah");
+            DataTable table = new DataTable();
+            table.Columns.Add("Link");
+            table.Columns.Add("Date");
+            table.Columns.Add("Type");
+            table.Rows.Add(DownloadManager.DeactivationURL, DateTime.Now.ToString(), "Update");
+
+            ServiceManager.Start(table);
+            bool found = ServiceManager.CheckForNewDeactivations();
+            Assert.IsTrue(found);
         }
 
         [TestMethod]
