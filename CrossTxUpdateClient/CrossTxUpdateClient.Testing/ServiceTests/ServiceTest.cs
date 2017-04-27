56f49dc6d5b38125ab6cf4c9c603819c55d01d18
Lane Lutgen <lutgenlane@gmail.com>
2017-04-26 18:27:14 -0600

    Added some service tests

--- CrossTxUpdateClient/CrossTxUpdateClient.Testing/ServiceTests/ServiceTest.cs
+++ CrossTxUpdateClient/CrossTxUpdateClient.Testing/ServiceTests/ServiceTest.cs
@@ -1,14 +1,55 @@
 ﻿using System;
+using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrossTxUpdateClient.Services;
 
 namespace CrossTxUpdateClient.Testing.ServiceTests
 {
     [TestClass]
     public class ServiceTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void StartService()
         {
+            ServiceManager.Start(new System.Data.DataTable());
+        }
+
+        [TestMethod]
+        public void CheckForNewUpdates_Test()
+        {
+            bool found = ServiceManager.CheckForNewUpdates();
+            Assert.IsFalse(found);
+        }
+
+        [TestMethod]
+        public void CheckForNewDeactivations_Test()
+        {
+            bool found = ServiceManager.CheckForNewDeactivations();
+            Assert.IsFalse(found);
+        }
+
+        [TestMethod]
+        public void AutoDownloadUpdate_Test()
+        {
+            ServiceManager.DownloadUpdateFile();
+
+            //No exceptions result
+        }
+
+        [TestMethod]
+        public void AutoDownloadDeactivation_Test()
+        {
+            ServiceManager.DownloadDeactivationFile();
+
+            //No exceptions result
+        }
+
+        [TestMethod]
+        public void StopServiceManager_Test()
+        {
+            ServiceManager.Stop();
+
+            //No exceptions result
         }
     }
 }
